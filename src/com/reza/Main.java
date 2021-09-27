3437bb1a5ce3f4389c5cb6f2414f520c4e859183
Naim Reza <naim.reza@cellosco.pe>
2021-09-27 11:14:34 +0600

    write test

--- src/com/reza/Main.java
+++ src/com/reza/Main.java
@@ -1,8 +1,31 @@
 package com.reza;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //create instance of EncodeFile and DecodeFile class
+        EncodeFile encoder = new EncodeFile();
+        DecodeFile decoder = new DecodeFile();
+        try{
+            //get input option and filename
+            String option = args[0];
+            File file = new File(args[1]);
+
+            //execute corresponding actions
+            switch (option){
+                case "encode":
+                    encoder.encode(file);
+                    break;
+                case "decode":
+                    decoder.decode(file);
+                    break;
+                default:
+                    System.out.println("Invalid option!! Try again.");
+            }
+        } catch (ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
     }
 }
